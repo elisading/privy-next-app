7081aab6f9337797ccdb6bf84dc406c419b2c897
Jared Downing <jareddowning@Jareds-MacBook-Pro.local>
2024-10-02 19:33:00 -0400

    stringify data 2

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -280,7 +280,7 @@ export default function DashboardPage() {
         {ready && authenticated ? (
           <>
             <div className="flex flex-row justify-between">
-              <h1 className="text-2xl font-semibold">Gamebets</h1>
+              <h1 className="text-2xl font-semibold">Gamebetszz</h1>
               <button
                 onClick={logout}
                 className="text-sm bg-violet-200 hover:text-violet-900 py-2 px-4 rounded-md text-violet-700"
