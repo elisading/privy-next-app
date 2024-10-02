64d615710970946c18e7902873f8243bd7f636b1
elisading <elisa.ding@hotmail.com>
2024-10-02 15:57:52 -0400

    fix build

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -150,7 +150,7 @@ export default function DashboardPage() {
     // Check that your user is authenticated
     const isAuthenticated = ready && authenticated;
     // Check that your user has an embedded wallet
-    const hasEmbeddedWallet = !!user.linkedAccounts.find(
+    const hasEmbeddedWallet = !!user?.linkedAccounts?.find(
       (account): account is WalletWithMetadata =>
         account.type === 'wallet' &&
         account.walletClientType === 'privy' &&
